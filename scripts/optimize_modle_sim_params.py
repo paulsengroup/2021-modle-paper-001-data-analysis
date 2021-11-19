ee2c664f7311040b4a4341b94e47f0f3d85a594a
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-11-20 00:08:05 +0100

    Update param optimization script

--- scripts/optimize_modle_sim_params.py
+++ scripts/optimize_modle_sim_params.py
@@ -4,11 +4,10 @@ import argparse
 import glob
 import os
 import pickle
-import shutil
 import subprocess as sp
 import sys
 import uuid
-from multiprocessing import cpu_count
+import tempfile
 
 import numpy as np
 import pandas as pd
@@ -24,23 +23,6 @@ def try_convert_to_numeric(x):
         return float(x)
     except Exception:
         return x
-
-
-def compute_occupancy(poo, pnn):
-    # poo prob. occ -> occ
-    # pnn prob. nocc -> nocc
-    pno = 1 - pnn
-    pon = 1 - poo
-    occ = pno / (pon + pno)
-    return np.clip(occ, 0, 1)
-
-
-def compute_poo(occ, pnn):
-    # poo prob. occ -> occ
-    # pnn prob. nocc -> nocc
-    pno = 1 - pnn
-    pon = (pno - (occ * pno)) / occ
-    return np.clip(1 - pon, 0, 1)
 
 
 def eval(bed_file, bwig1, bwig2):
@@ -84,7 +66,7 @@ def run_subprocess(cmd):
 
 
 def run_modle(params):
-    cmd = [modle_bin, "sim",
+    cmd = ["modle", "sim",
            "-c", chrom_sizes,
            "--extrusion-barrier-file", extr_barriers,
            "-o", modle_out_prefix,
@@ -100,7 +82,7 @@ def run_modle(params):
 
 
 def run_modle_tools_transform(input_name, output_name, sigma, sigma_mult, cutoff):
-    cmd = [modle_tools_bin, "transform",
+    cmd = ["modle_tools", "transform",
            "-i", input_name,
            "--bin-size", str(bin_size),
            "-m", "difference_of_gaussians",
@@ -114,7 +96,7 @@ def run_modle_tools_transform(input_name, output_name, sigma, sigma_mult, cutoff
 
 
 def run_modle_tools_eval(input_name, output_name):
-    cmd = [modle_tools_bin, "eval",
+    cmd = ["modle_tools", "eval",
            "-i", input_name,
            "--bin-size", str(bin_size),
            "--reference-matrix", reference_matrix_transformed,
@@ -158,32 +140,31 @@ def fx(params):
 
 def make_cli():
     cli = argparse.ArgumentParser()
-    cli.add_argument("--bin-dir",
-                     help="Path to the root of a cmake build directory containing modle and modle_tools binaries.",
-                     required=True)
     cli.add_argument("--param-space-tsv", help="Path to a TSV file with three columns names param, start and end.",
                      required=True)
-    cli.add_argument("--x0", help="Comma-separated string of starting points")
     cli.add_argument("--output-prefix", help="Output prefix", required=True)
-    cli.add_argument(
-        "--data-dir", help="Path to input data folder", required=True)
-    cli.add_argument("--eval-sites-bed",
-                     help="Path to a BED file with the genomic coordinates to use during evaluation", required=True)
-    cli.add_argument(
-        "--tmp-dir", help="Path to temporary directory", default="/tmp")
+    cli.add_argument("--chrom-sizes", help="Path to chrom.sizes file", required=True)
+    cli.add_argument("--extrusion-barriers", help="Path to extrusion barrier BED file", required=True)
+    cli.add_argument("--reference-matrix", help="Path to multi-res cooler to use as reference", required=True)
+    cli.add_argument("--evaluation-sites", help="Path to a BED file with the genomic coordinates to use during evaluation", required=True)
+
+    cli.add_argument("--x0", help="Comma-separated string of starting points")
+
     cli.add_argument("--excluded-chroms", default=["chrY", "chrM"], nargs="*")
-    cli.add_argument("--gaussian-blur-sigma_ref", default=2.0, type=float)
+    cli.add_argument("--gaussian-blur-sigma-ref", default=2.0, type=float)
     cli.add_argument("--gaussian-blur-sigma-multiplier_ref",
                      default=1.6, type=float)
-    cli.add_argument("--discretization-thresh_ref", default=1.5, type=float)
-    cli.add_argument("--gaussian-blur-sigma_tgt", default=1.0, type=float)
-    cli.add_argument("--gaussian-blur-sigma-multiplier_tgt",
+    cli.add_argument("--discretization-thresh-ref", default=1.5, type=float)
+    cli.add_argument("--gaussian-blur-sigma-tgt", default=1.0, type=float)
+    cli.add_argument("--gaussian-blur-sigma-multiplier-tgt",
                      default=1.6, type=float)
-    cli.add_argument("--discretization-thresh_tgt", default=1.0, type=float)
+    cli.add_argument("--discretization-thresh-tgt", default=1.0, type=float)
+    cli.add_argument("--diagonal-width", default=int(3e6), type=int)
+
     cli.add_argument("--ncalls", default=200, type=int)
     cli.add_argument("--nrandom-starts", default=10, type=int)
-    cli.add_argument("--method", default="bayesian", choices=["dummy", "forest", "gbrt", "bayesian"],
-                     help="https://scikit-optimize.github.io/stable/modules/minimize_functions.html#skopt.forest_minimize")
+    cli.add_argument("--optimization-method", default="bayesian", choices=["dummy", "forest", "gbrt", "bayesian"],
+                     help="See https://scikit-optimize.github.io/stable/modules/minimize_functions.html#skopt.forest_minimize")
     cli.add_argument("--seed", default=1630986062, type=int)
     cli.add_argument("--modle-tools-eval-metric", default="custom",
                      choices=["custom", "eucl_dist", "pearson", "rmse", "spearman"])
@@ -194,37 +175,24 @@ def make_cli():
 if __name__ == "__main__":
     args = make_cli().parse_args()
 
-    bin_dir = args.bin_dir
-    modle_bin = f"{bin_dir}/src/modle/modle"
-    modle_tools_bin = f"{bin_dir}/src/modle_tools/modle_tools"
-
     data_dir = args.data_dir
     tmpdir = args.tmp_dir
-    tmp_prefix = f"{tmpdir}/modle_sim_opt/{args.method}_opt"
     out_prefix = args.output_prefix
 
     uuid_ = uuid.uuid1()
 
     param_df = pd.read_csv(args.param_space_tsv, sep="\t")
     param_df.set_index("param", inplace=True)
-    assert param_df.loc["--bin-size",
-                        "start"] == param_df.loc["--bin-size", "end"]
+    assert param_df.loc["--bin-size", "start"] == param_df.loc["--bin-size", "end"]
+
     bin_size = int(param_df.loc["--bin-size", "start"])
-    diagonal_width = int(3e6)
-    chrom_sizes = os.path.expanduser(
-        f"{data_dir}/chr_sizes/GRCh38.chrom.sizes")
-    extr_barriers = os.path.expanduser(
-        f"{data_dir}/extr_barriers/GRCh38_H1_CTCF_sites_filtered.bed.gz")
-    reference_matrix = os.path.expanduser(
-        f"{data_dir}/coolers/4DNFIBM9QCFG.mcool")
-    reference_matrix_transformed = os.path.expanduser(
-        f"{data_dir}/coolers/4DNFIBM9QCFG_transformed.cool")
-    modle_out_prefix = os.path.expanduser(
-        f"{tmp_prefix}/modle/hg38_opt_{uuid_}")
-    modle_tools_out_prefix = os.path.expanduser(
-        f"{tmp_prefix}/modle_tools/hg38_opt_{uuid_}")
+    diagonal_width = int(args.diagonal_width)
+    chrom_sizes = args.chrom_sizes
+    extr_barriers = args.extrusion_barriers
+    reference_matrix = args.reference_matrix
+    reference_matrix_transformed = reference_matrix.replace(".mcool", "_transformed.cool")
     excluded_chroms = set(args.excluded_chroms)
-    nthreads = cpu_count()
+    nthreads = args.nthreads
     seed = args.seed
 
     gaussian_blur_sigma_ref = args.gaussian_blur_sigma_ref
@@ -246,7 +214,7 @@ if __name__ == "__main__":
     nrandom_starts = args.nrandom_starts
 
     optimizer = None
-    method = args.method
+    method = args.optimization_method
     if method == "dummy":
         optimizer = dummy_minimize
     elif method == "forest":
@@ -257,11 +225,10 @@ if __name__ == "__main__":
         assert method == "bayesian"
         optimizer = gp_minimize
 
-    try:
-        os.makedirs(os.path.dirname(tmp_prefix), exist_ok=True)
+    with tempfile.TemporaryDirectory(suffix=f"optimize_modle_sim_params_{method}") as tmpdir:
         os.makedirs(os.path.dirname(out_prefix), exist_ok=True)
-
-        sp.run(["cmake", "--build", ".", "-j", str(cpu_count())], cwd=bin_dir)
+        modle_out_prefix = f"{tmpdir}/modle_"
+        modle_tools_out_prefix = f"{tmpdir}/modle_tools_"
 
         run_modle_tools_transform(reference_matrix, reference_matrix_transformed, gaussian_blur_sigma_ref,
                                   gaussian_blur_sigma_multiplier_ref, discretization_thresh_ref)
@@ -299,10 +266,5 @@ if __name__ == "__main__":
                         n_random_starts=nrandom_starts,
                         random_state=seed)
 
-        shutil.rmtree(tmp_prefix)
         with open(f"{out_prefix}_{method}.pickle", "wb") as fp:
             pickle.dump(res, fp)
-
-    finally:
-        if os.path.exists(tmp_prefix):
-            shutil.rmtree(tmp_prefix)
