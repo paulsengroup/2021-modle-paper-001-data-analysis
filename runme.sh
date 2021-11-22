660b71530d4cfc401e2dc7c1d62d42b716103b15
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-11-22 17:28:26 +0100

    Add runme.sh

---
+++ runme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+nextflow run -c configs/fetch_data.config \
+    workflows/fetch_data.nf \
+    -resume
+
+nextflow run -c configs/preprocess_data.config \
+    workflows/preprocess_data.nf \
+    -resume
+
+nextflow run -c configs/optimize_modle_sim_params.config \
+    workflows/optimize_modle_sim_params.nf \
+    -resume
