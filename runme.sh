7fc5fc59970fa091a03e929ce04bff2fad00fc9e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-04-08 14:03:22 +0200

    Update run_me.sh script

--- runme.sh
+++ runme.sh
@@ -11,8 +11,8 @@ function run_workflow() {
     if [[ $HOSTNAME == *.saga ]]; then
         base_config='configs/base_saga.config'
         args=("${@:2}"
-              --max_memory=3000.G
-              --max_cpus=64
+              --max_memory=400.G
+              --max_cpus=52
               --max_time=336.h
               --project="${SLURM_PROJECT_ID-changeme}")
     else
@@ -31,10 +31,31 @@ function run_workflow() {
 
 steps=(fetch_data
        preprocess_data
+       make_heatmaps_comparison
        optimize_modle_sim_params
-#       genome_wide_analyses
 )
 
 for step in "${steps[@]}"; do
     run_workflow "$step"
 done
+
+if [[ $HOSTNAME == *.saga ]]; then
+    mkdir -p .nextflow-benchmarks-wd
+    (cd .nextflow-benchmarks-wd && ln -sf ../configs/ configs)
+    (cd .nextflow-benchmarks-wd && ln -sf ../containers/ containers)
+    (cd .nextflow-benchmarks-wd && ln -sf ../data/ data)
+    (cd .nextflow-benchmarks-wd && ln -sf ../scripts/ scripts)
+    (cd .nextflow-benchmarks-wd && ln -sf ../workflows/ workflows)
+
+    (cd .nextflow-benchmarks-wd && \
+        nextflow run \
+            --max_memory=400.G \
+            --max_cpus=52 \
+            --max_time=336.h \
+            --project="${SLURM_PROJECT_ID-changeme}" \
+            -profile benchmark \
+            -c "configs/run_benchmarks.config" \
+            -c "configs/base_saga.config" \
+            "workflows/run_benchmarks.nf" \
+            -resume)
+fi
