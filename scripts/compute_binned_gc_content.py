331d4e315a5fa7e850925876b2788afd261e015e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-08-21 21:55:38 +0200

    Add missing script used in preprocess_data workflow

---
+++ scripts/compute_binned_gc_content.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import bioframe as bf
+import sys
+
+
+def make_cli():
+    cli = argparse.ArgumentParser()
+
+    cli.add_argument("bins")
+    cli.add_argument("genome_assembly")
+
+    return cli
+
+
+if __name__ == "__main__":
+    args = vars(make_cli().parse_args())
+
+    df = bf.frac_gc(bf.read_table(args["bins"], schema="bed3"),
+                    bf.load_fasta(args["genome_assembly"]))
+
+    df.to_csv(sys.stdout, index=False, header=False, sep="\t")
