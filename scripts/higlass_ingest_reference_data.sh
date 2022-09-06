4aeb05d162ea140465149b8b44916b59c9756e0a
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-09-06 19:33:09 +0200

    Add script to ingest reference data into HiGlass

---
+++ scripts/higlass_ingest_reference_data.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+git_root="$(git rev-parse --show-toplevel)"
+name="$(basename "$git_root")"
+
+data_dir="$git_root/data/"
+higlass_dir="$data_dir/higlass/"
+
+# rm -rf "$higlass_dir"
+
+"$(dirname "$0")/higlass_start_instance.sh"
+
+function uuidgen {
+    "$(dirname "$0")/higlass_uuid_generator.py" "$1"
+}
+
+export uuidgen
+
+# Ingest chrom sizes
+sudo -E higlass-manage ingest \
+    --filetype chromsizes-tsv \
+    --project-name "Chromosomes" \
+    --datatype chromsizes \
+    --assembly hg38 \
+    --name GRCh38 \
+    --uid "$(uuidgen "GRCh38.chrom.sizes")" \
+    --no-upload \
+    --hg-name "$name" \
+    output/preprocessing/chrom_sizes/GRCh38.chrom.sizes
+
+sudo -E higlass-manage ingest \
+    --filetype chromsizes-tsv \
+    --project-name "Chromosomes" \
+    --datatype chromsizes \
+    --assembly hg19 \
+    --name GRCh37 \
+    --uid "$(uuidgen "GRCh37.chrom.sizes")" \
+    --no-upload \
+    --hg-name "$name" \
+    output/preprocessing/chrom_sizes/GRCh37.chrom.sizes
+
+sudo -E higlass-manage ingest \
+    --filetype chromsizes-tsv \
+    --project-name "Chromosomes" \
+    --datatype chromsizes \
+    --assembly mm10 \
+    --name GRCm38 \
+    --uid "$(uuidgen "GRCm38.chrom.sizes")" \
+    --no-upload \
+    --hg-name "$name" \
+    output/preprocessing/chrom_sizes/GRCm38.chrom.sizes
+
+# Ingest reference matrices
+for mcool in "$data_dir/output/preprocessing/mcools/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "Reference" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/preprocessing/mcools/$mcool_name.mcool"
+done
+
+# Ingest ChIP bigwig files
+for bw in "$data_dir/input/"GRCh37*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly hg19 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "input/$bw_name.bigwig"
+done
+
+for bw in "$data_dir/input/"GRCh38*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly hg38 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "input/$bw_name.bigwig"
+done
+
+for bw in "$data_dir/output/hoxd_encode_chip/"*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly mm10 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/hoxd_encode_chip/$bw_name.bigwig"
+done
