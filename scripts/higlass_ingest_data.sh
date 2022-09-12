ecaa714cfeb1ca8d610bd6dbf4b6c08ec98e3329
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-09-12 18:22:45 +0200

    Update scripts to ingest data into HiGlass

--- scripts/higlass_ingest_data.sh
+++ scripts/higlass_ingest_data.sh
@@ -10,7 +10,7 @@ name="$(basename "$git_root")"
 data_dir="$git_root/data/"
 higlass_dir="$data_dir/higlass/"
 
-rm -rf "$higlass_dir"
+# rm -rf "$higlass_dir"
 
 "$(dirname "$0")/higlass_start_instance.sh"
 
@@ -66,6 +66,57 @@ for mcool in "$data_dir/output/preprocessing/mcools/"*.mcool; do
         "output/preprocessing/mcools/$mcool_name.mcool"
 done
 
+# Ingest ChIP bigwig files
+for bw in "$data_dir/input/"GRCh37*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly hg19 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "input/$bw_name.bigwig"
+done
+
+for bw in "$data_dir/input/"GRCh38*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly hg38 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "input/$bw_name.bigwig"
+done
+
+for bw in "$data_dir/input/"GRCm38*.bigwig; do
+    bw_name="$(basename "$bw" .bigwig)"
+    sudo -E higlass-manage ingest \
+        --project-name "ChIP" \
+        --name "$bw_name" \
+        --assembly mm10 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "input/$bw_name.bigwig"
+done
+
+# Ingest matrices for heatmap-comparison pt1
+for mcool in "$data_dir/output/heatmap_comparison_pt1/matrices/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "heatmap_comparison_pt1" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/heatmap_comparison_pt1/matrices/$mcool_name.mcool"
+done
+
+exit 0
+
 # Ingest matrices for genome-wide param optimization
 for mcool in "$data_dir/output/gw_param_optimization/mcools/"*.mcool; do
     mcool_name="$(basename "$mcool" .mcool)"
@@ -78,12 +129,90 @@ for mcool in "$data_dir/output/gw_param_optimization/mcools/"*.mcool; do
         "output/gw_param_optimization/mcools/$mcool_name.mcool"
 done
 
-# matrices=("$data_dir/$name/matrices/"*.mcool "$data_dir/preprocessing/mcools/"GRCh38_H1_hESC*.mcool "$wd/data/media/uploads/"*.mcool)
-# for m in "${matrices[@]}"; do
-#     sudo -E higlass-manage ingest \
-#         --assembly hg38 \
-#         --name "$(basename "$m" .mcool)" \
-#         "$(basename "$m")" \
-#         --hg-name "$name" \
-#         --no-upload
-# done
+exit 0
+
+# Ingest extrusion barrier param optimization data
+for bw in "$data_dir/output/extrusion_barrier_param_optimization/optimized_barriers/"*.bw; do
+    bw_name="$(basename "$bw" .bw)"
+    sudo -E higlass-manage ingest \
+        --project-name "extrusion_barrier_param_optimization" \
+        --name "$bw_name" \
+        --assembly hg38 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/extrusion_barrier_param_optimization/optimized_barriers/$bw_name.bw"
+done
+
+# Ingest extrusion barrier param optimization data
+for mcool in "$data_dir/output/extrusion_barrier_param_optimization/mcools/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "extrusion_barrier_param_optimization" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/extrusion_barrier_param_optimization/mcools/$mcool_name.mcool" &
+done
+wait
+
+bw_name="GRCh38_H1_optimized_barriers_microc_003_extrusion_barriers_lef_1d_occupancy"
+sudo -E higlass-manage ingest \
+    --project-name "extrusion_barrier_param_optimization" \
+    --name "$bw_name" \
+    --assembly hg38 \
+    --uid "$(uuidgen "$bw_name")" \
+    --hg-name "$name" \
+    --no-upload \
+    "output/extrusion_barrier_param_optimization/simulations/$bw_name.bw"
+
+# Ingest extrusion barrier param optimization data
+for bw in "$data_dir/output/extrusion_barrier_param_optimization/optimized_barriers/"*.bw; do
+    bw_name="$(basename "$bw" .bw)"
+    sudo -E higlass-manage ingest \
+        --project-name "extrusion_barrier_param_optimization" \
+        --name "$bw_name" \
+        --assembly hg38 \
+        --uid "$(uuidgen "$bw_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/extrusion_barrier_param_optimization/optimized_barriers/$bw_name.bw"
+done
+
+exit 0
+
+# Ingest matrices for genome-wide param optimization
+for mcool in "$data_dir/output/gw_param_optimization/mcools/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "gw_param_optimization" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/gw_param_optimization/mcools/$mcool_name.mcool"
+done
+
+# Ingest matrices for heatmap-comparison pt2
+for mcool in "$data_dir/output/heatmap_comparison_pt2/prob_decay/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "heatmap_comparison_pt2_prob_decay" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/heatmap_comparison_pt2/prob_decay/$mcool_name.mcool"
+done
+
+for mcool in "$data_dir/output/heatmap_comparison_pt2/lef_param_exploration/mcools/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "heatmap_comparison_pt2_lef_param_exploration" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/heatmap_comparison_pt2/lef_param_exploration/mcools/$mcool_name.mcool"
+done
---
+++ scripts/higlass_ingest_data_comparison_with_mut.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+git_root="$(git rev-parse --show-toplevel)"
+name="$(basename "$git_root")"
+
+data_dir="$git_root/data/"
+higlass_dir="$data_dir/higlass/"
+
+# rm -rf "$higlass_dir"
+
+"$(dirname "$0")/higlass_start_instance.sh"
+
+function uuidgen {
+    "$(dirname "$0")/higlass_uuid_generator.py" "$1"
+}
+
+export uuidgen
+
+# Ingest comparison with mutant data
+for mcool in "$data_dir/output/comparison_with_mut/mcools/"*.mcool; do
+    mcool_name="$(basename "$mcool" .mcool)"
+    sudo -E higlass-manage ingest \
+        --project-name "comparison_with_mut" \
+        --name "$mcool_name" \
+        --uid "$(uuidgen "$mcool_name")" \
+        --hg-name "$name" \
+        --no-upload \
+        "output/comparison_with_mut/mcools/$mcool_name.mcool" &
+done
+wait
